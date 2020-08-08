a271a2957601c82ce8d6d4da230db1978b426b8f
signalhunter <boom@flyingpackets.net>
2020-08-08 02:52:36 -0400

    that typo surely took a while to be noticed

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@
 git clone -b next https://github.com/tmpim/juroku
 
 # Build it
-docker pull upsilondev/golang-gcc:latest
-docker run --rm -u 1000 -v "$PWD/juroku":/tmp/juroku -w /tmp/juroku/stream/server \
--e GOCACHE='/tmp/go' -e GO_CFLAGS_ALLOW='.*' -e CGO_LDFLAGS_ALLOW='.*' -e CC='gcc' upsilondev/golang-gcc:latest go build
+docker pull golang
+docker run --rm -v "$PWD/juroku":/tmp/juroku -w /tmp/juroku/stream/server \
+-e CGO_CFLAGS_ALLOW='.*' -e CGO_LDFLAGS_ALLOW='.*' -e CC='gcc' upsilondev/golang-gcc:latest go build
 
 # Cleanup
-#mv juroku/stream/server/server .
-#rm -rf juroku/
+mv juroku/stream/server/server juroku-server
+strip juroku-server
+rm -rf juroku/
