0a735fd664e4902837d337f96348f93f6436316c
signalhunter <boom@flyingpackets.net>
2020-08-08 03:17:23 -0400

    use official golang image

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@
 git clone -b next https://github.com/tmpim/juroku
 
 # Build it
-docker pull golang
 docker run --rm -v "$PWD/juroku":/tmp/juroku -w /tmp/juroku/stream/server \
--e CGO_CFLAGS_ALLOW='.*' -e CGO_LDFLAGS_ALLOW='.*' -e CC='gcc' upsilondev/golang-gcc:latest go build
+-e CGO_CFLAGS_ALLOW='.*' -e CGO_LDFLAGS_ALLOW='.*' -e CC='gcc' golang go build
 
 # Cleanup
 mv juroku/stream/server/server juroku-server
